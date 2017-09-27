ff8c27c2a44b4f389f84ed35ee415c41d3a555aa
Samuel Trostle <samtrostle@gmail.com>
2017-09-27 15:23:30 -0400

    Update DogYearsCalculator.py

--- DogYearsCalculator.py
+++ DogYearsCalculator.py
@@ -1,6 +1,5 @@
-#Samuel Trostle, Assignment 1
+#Samuel Trostle
 import time
-
 firstname = input("What is your first name?: ")
 lastname = input("What is your last name?: ")
 age = input("What is your age?: ")
@@ -10,3 +9,4 @@ time.sleep(0.6)
 print("...")
 time.sleep(0.5)
 print("Hi, " + firstname + " " + lastname + ", you may be " + age + " years old but in dog years you are " + str(dogage) + " old so get busy living!")
+exit = input("Press any key to exit")
