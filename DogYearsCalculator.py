60e2b219c219e5cf04b35ca6bb1c2c1fbe28ffc8
Samuel Trostle <samtrostle@gmail.com>
2017-09-27 15:15:14 -0400

    Add files via upload

---
+++ DogYearsCalculator.py
@@ -0,0 +1,12 @@
+#Samuel Trostle, Assignment 1
+import time
+
+firstname = input("What is your first name?: ")
+lastname = input("What is your last name?: ")
+age = input("What is your age?: ")
+dogage =  int(age)*7
+print("barkulating your age...")
+time.sleep(0.6)
+print("...")
+time.sleep(0.5)
+print("Hi, " + firstname + " " + lastname + ", you may be " + age + " years old but in dog years you are " + str(dogage) + " old so get busy living!")
